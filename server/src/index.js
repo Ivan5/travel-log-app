1ede84422c619c27fbd9d4c57ab9adeadd6ffb95
Ivan5 <converse_ivan@hotmail.com>
2020-02-25 14:56:31 -0600

    refactor the main file and move the middleware func in a other file

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,31 @@
 const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
+
+const middlewares = require("./middlewares");
 
 const app = express();
 
-const port = process.env.PORT || 1331;
+//middlewares
+app.use(morgan("common")); //use to describe the request
+app.use(helmet()); //use to remove and add some headers to the page to prevent attacs
+app.use(
+  cors({
+    origin: "http://localhost:3000"
+  })
+);
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello Wordl" });
+});
+
+app.use(middlewares.notFound);
+
+app.use(middlewares.errorHandler);
+
+const port = process.env.PORT || 1337;
 
 app.listen(port, function() {
-  console.log(`listening at http://localhost:1337`);
+  console.log(`listening at http://localhost:${port}`);
 });
